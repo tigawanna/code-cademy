f26ad79232fcc0a11e7bb602ea966987e5e042d5
tigawanna <denniskinuthiaw@gmail.com>
2022-06-19 10:01:49 +0300

    telephone checker

--- cypher.js
+++ cypher.js
@@ -2,23 +2,26 @@ function rot13(str) {
     let filtered =[]
     const cypher="NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm"
     const alpha=  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+const spcChar=[" ","?","!","#","$","%","."]
 
-    console.log(cypher[7])
-    const stuff= str.replace(/[A-Z]/gi, c =>filtered.push(c))
+    const stuff= str.replace(/[A-Z]|\s+|!|\?|\./gi, c =>filtered.push(c))
     console.log("stuff ==== ",filtered)
     let reversed =[];
     filtered.forEach((item)=>{
-      console.log(alpha.indexOf(item))  
-      reversed.push(cypher[alpha.indexOf(item)])
-      
-      
-    })
-    console.log(reversed)
+    const specialIndex=spcChar.findIndex((x)=>x===item)
+    if(specialIndex!==-1){
+        reversed.push(spcChar[specialIndex])
+      } else{
+        reversed.push(cypher[alpha.indexOf(item)])
+      } 
+   })
+
+    console.log("reversed ==== ",reversed)
     let final=""
     reversed.forEach((item)=>final+=item)
     return final
   }
 
- console.log(rot13("HEY")) 
+ console.log(rot13("GUR DHVPX OEBJA SBK WHZCF BIRE GUR YNML QBT.")) 
 
  
