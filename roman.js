21f81b59f80ae3296a5fe56210857d995963f5c2
tigawanna <denniskinuthiaw@gmail.com>
2022-06-20 16:09:00 +0300

    roman

---
+++ roman.js
@@ -0,0 +1,55 @@
+function convertToRoman(num) {
+    const  romanArr = [
+       ["M",1000],
+      [ "CM",900],
+      [ "D", 500],
+      [ "CD", 400],
+        ["C",100],
+        ["XC", 90],
+        ["L" ,50],
+        ["X", 40],
+        ["X", 10],
+        ["IX", 9],
+        ["V", 5],
+        ["IV", 4],
+        ["I", 1]
+    ];
+
+    const lookupTable = {
+      M: 1000,
+      CM: 900,
+      D: 500,
+      CD: 400,
+      C: 100,
+      XC: 90,
+      L: 50,
+      XL: 40,
+      X: 10,
+      IX: 9,
+      V: 5,
+      IV: 4,
+      I: 1
+    };
+  
+    let accumulator = '';
+  
+    for (const key in lookupTable) {
+      const numberValue = lookupTable[key];
+  
+      while (numberValue <= num) {
+        num -= numberValue;
+        accumulator += key;
+      }
+    }
+  
+    return accumulator;
+  }
+  
+for(i=0;i<10;i++){
+console.log(`Roman for number ${i}`,convertToRoman(i));
+}
+
+for(i=10;i<100;i+=10){
+console.log(`Roman for number ${i}`,convertToRoman(i));
+}
+  
