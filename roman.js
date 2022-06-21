38293b00d2165fcdb9c9390ff7ebdf425459c0c2
tigawanna <denniskinuthiaw@gmail.com>
2022-06-21 06:24:59 +0300

    roman

--- roman.js
+++ roman.js
@@ -1,13 +1,13 @@
 function convertToRoman(num) {
     const  romanArr = [
        ["M",1000],
-      [ "CM",900],
-      [ "D", 500],
-      [ "CD", 400],
+       [ "CM",900],
+       [ "D", 500],
+       [ "CD", 400],
         ["C",100],
         ["XC", 90],
         ["L" ,50],
-        ["X", 40],
+        ["XL", 40],
         ["X", 10],
         ["IX", 9],
         ["V", 5],
@@ -15,41 +15,46 @@ function convertToRoman(num) {
         ["I", 1]
     ];
 
-    const lookupTable = {
-      M: 1000,
-      CM: 900,
-      D: 500,
-      CD: 400,
-      C: 100,
-      XC: 90,
-      L: 50,
-      XL: 40,
-      X: 10,
-      IX: 9,
-      V: 5,
-      IV: 4,
-      I: 1
-    };
+
   
     let accumulator = '';
+
+    // for(i=0 ; i<romanArr.length; i++){
+    //     const romanValue = romanArr[i][0]
+    //     const arabicEquiv =romanArr[i][1]
+
+    // while (arabicEquiv <= num) {
+    //     num -= arabicEquiv;
+    //     accumulator += romanValue;
+
+    //     console.log("num === ",num)
+    //     console.log("roman value === ",romanValue)
+    //     console.log("accumulator === ",accumulator)
+    //   }
+    // }
+   romanArr.forEach(([romanValue,arabicEquiv])=>{
   
-    for (const key in lookupTable) {
-      const numberValue = lookupTable[key];
-  
-      while (numberValue <= num) {
-        num -= numberValue;
-        accumulator += key;
-      }
-    }
-  
+        while (arabicEquiv <= num) {
+        num -= arabicEquiv;
+        accumulator += romanValue;
+       }
+
+  })
+
+
+
     return accumulator;
   }
   
-for(i=0;i<10;i++){
-console.log(`Roman for number ${i}`,convertToRoman(i));
-}
 
-for(i=10;i<100;i+=10){
-console.log(`Roman for number ${i}`,convertToRoman(i));
-}
+  console.log(`Roman for number 312`,convertToRoman(44));
+
+
+// for(i=0;i<10;i++){
+// console.log(`Roman for number ${i}`,convertToRoman(i));
+// }
+
+// for(i=10;i<100;i+=10){
+// console.log(`Roman for number ${i}`,convertToRoman(i));
+// }
   
